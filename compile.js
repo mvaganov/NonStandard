8af95020589e62c3e0f0c39ab5964337503fa2eb
Michael Vaganov <mvaganov@hotmail.com>
2022-04-05 22:06:10 -0700

    Update compile.js

--- compile.js
+++ compile.js
@@ -1,47 +1,98 @@
 'use strict';
-
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('ls.json');
-let listing = JSON.parse(rawdata);
-let mapping = {};
-for (let i = 0; i < listing.length; i++) {
-	mapping[listing[i].packid] = listing[i];
-}
-let requestedPackages = ['*'];
-const gitimportFilename = "gitimport.bat";
+/** list of all packages in ths system */
+let g_packageListing = [];
+/** all packages in ths system by id */
+let g_packageMapping = {};
+/** list of packages the client app wants */
+let g_requestedPackages = ['*'];
+
+const gitImportBatchFilename = "gitimport.bat";
 const packageGitignore = "../.gitignore";
 const nonstandardManifest = "../nonstandardmanifest.json";
-let extraLibGitignore = "";
-let gitimport = "cd ..\n";
-let libgitignore = null;
 
-DoThese([
-	prepareNonStandardManifest,
-	prepareToAddToGitignore,
-	writePackageJsonFiles,
-	determineWhichPackagesToImportFromGit,
-	writeGitimportBatchfile,
-	writeGitignore,
-	function(callback) { console.log(_colorCyan("finished package metadata compile")); callback(null); }
-]);
-
-function DoThese(thingsToDo) {
+/** 
+* handy dandy linear promise helper function.
+* @param {Function(error)[]} thingsToDoInOrder a list of things to do, in order
+*/
+function DoThese(thingsToDoInOrder) {
 	function DoThem(err) {
-		if (!err) {
-			if (thingsToDo.length == 0) { return; }
-			let nextThing = thingsToDo[0];
-			thingsToDo = thingsToDo.slice(1);
-			//console.log(`about to do thing ${thingsToDoIndex}: ${thingsToDo[thingsToDoIndex]}`);
-			nextThing(DoThem);
-		} else {
-			throw err;
-		}
+		if (err) { throw err; }
+		if (thingsToDoInOrder.length == 0) { return; }
+		let nextThingToDo = thingsToDoInOrder[0];
+		thingsToDoInOrder = thingsToDoInOrder.slice(1);
+		nextThingToDo(DoThem);
 	}
 	DoThem();
 }
 
-function prepareNonStandardManifest(callback) {
+let _doAll = [
+	loadPackageListing,
+	loadRequestedPackages,
+	writePackageJsonFiles,
+	determineWhichPackagesToGitimport,
+	writeGitimportBatchfile,
+	writeGitignore
+];
+let _doPackageUpdate = [
+	loadPackageListing,
+	writePackageJsonFiles,
+];
+let _doGenerateGitimportFiles = [
+	loadPackageListing,
+	loadRequestedPackages,
+	determineWhichPackagesToGitimport,
+	writeGitimportBatchfile,
+	writeGitignore
+];
+
+var instructionsFromArgs = {
+	'all': _doAll,
+	'package': _doPackageUpdate,
+	'p': _doPackageUpdate,
+	'import': _doGenerateGitimportFiles,
+	'i':_doGenerateGitimportFiles
+}
+
+let commandLineArgs = process.argv.slice(2);
+//console.log('arguments: ', commandLineArgs);
+if (commandLineArgs.length > 0) {
+	DoTheArgs(commandLineArgs);
+} else {
+	console.log(_colorRed(`missing argument, valid tokens: [${showValidCommands()}]`));
+}
+
+function showValidCommands() { return Object.keys(instructionsFromArgs).join(', '); }
+
+function DoTheArgs(args) {
+	function DoNextArgInstructionList(err) {
+		if (err) { throw err; }
+		if (!args || args.length == 0) { return; }
+		let arg = args[0];
+		args = args.slice(1);
+		let instructions = instructionsFromArgs[arg];
+		if (!instructions) {
+			console.log(_colorRed(`'${arg}' unknown token, valid tokens: [${showValidCommands()}]`));
+			return;
+		}
+		instructions = instructions.splice(0);
+		instructions.push(() => DoNextArgInstructionList());
+		DoThese(instructions);
+	}
+	DoNextArgInstructionList();
+}
+
+function loadPackageListing(callback) {
+	let rawdata = fs.readFileSync('ls.json');
+	g_packageListing = JSON.parse(rawdata);
+	for (let i = 0; i < g_packageListing.length; i++) {
+		g_packageMapping[g_packageListing[i].packid] = g_packageListing[i];
+	}
+	callback();
+}
+
+function loadRequestedPackages(callback) {
 	fs.readFile(nonstandardManifest, 'utf8' , (err, data) => {
 		if (err) {
 			requestAllPackages();
@@ -49,26 +100,26 @@ function prepareNonStandardManifest(callback) {
 			return;
 		}
 		let packageManifest = JSON.parse(data);
-		requestedPackages = packageManifest.req;//data.split('\n');
-		console.log(`${_colorCyan("need packages: ")} ${requestedPackages.join(", ")}`);
+		g_requestedPackages = packageManifest.req;
+		console.log(`${_colorCyan("need packages: ")} ${g_requestedPackages.join(", ")}`);
 		callback();
 	});
 }
 
 function requestAllPackages() {
-	requestedPackages = [];
-	for (let i = 0; i < listing.length; ++i) {
-		let packid = listing[i].packid;
+	g_requestedPackages = [];
+	for (let i = 0; i < g_packageListing.length; ++i) {
+		let packid = g_packageListing[i].packid;
 		if (!packid) {
-			console.log(_colorRed(`missing package id for ${listing[i]}`));
+			console.log(_colorRed(`missing package id for ${g_packageListing[i]}`));
 		}
-		requestedPackages.push(packid);
+		g_requestedPackages.push(packid);
 	}
 }
 
 function writeNonStandardManifest(callback) {
-	let reqs = requestedPackages.length > 0 ? 
-		'\n\t\t"' + requestedPackages.join('",\n\t\t"') + '"\n\t' : "";
+	let reqs = g_requestedPackages.length > 0 ? 
+		'\n\t\t"' + g_requestedPackages.join('",\n\t\t"') + '"\n\t' : "";
 	let nonstandardManifestText =
 `{
 	"req": [${reqs}]
@@ -79,41 +130,20 @@ function writeNonStandardManifest(callback) {
 	});
 }
 
-function prepareToAddToGitignore(callback) {
-	fs.readFile(packageGitignore, 'utf8' , (err, data) => {
-		if (err) {
-			console.error(err);
-			callback();
-			return;
-		}
-		libgitignore = data.split('\n');
-		for(let i = 0; i < libgitignore.length; i++) {
-			libgitignore[i] = libgitignore[i].replace(/\r/g, "");
-		}
-		console.log(`${_colorCyan("found")} ${packageGitignore}`);
-		callback();
-	});
-}
 function _colorCyan(str) { return `\x1b[36m${str}\x1b[0m`; }
 function _colorGreen(str) { return `\x1b[32m${str}\x1b[0m`; }
 function _colorRed(str) { return `\x1b[31m${str}\x1b[0m`; }
 function writePackageJsonFiles(callback) {
 	let written = 0, toWrite = 0;
 	let writtenFolders = [];
-	for (let i = 0; i < listing.length; ++i) {
-		let value = listing[i];
-		let key = value.packid;
+	for (let i = 0; i < g_packageListing.length; ++i) {
+		let value = g_packageListing[i];
 		if (value.listed === false) { continue; }
 		//console.log(`${key}: ${value}`);
 		let folderName = getFolderName(value);
 		let packageFolder = `../${folderName}`
 		let packagePath = packageFolder + '/package.json';
-		let packageText = CreatePackageFileData(value,mapping);
-		let extraIgnoreEntry = `${folderName}/`;
-		if (libgitignore == null || libgitignore.indexOf(extraIgnoreEntry) < 0) {
-			console.log(".gitignore does not contain "+extraIgnoreEntry);
-			extraLibGitignore += extraIgnoreEntry + "\n";
-		}
+		let packageText = CreatePackageFileData(value);
 		++toWrite;
 		writtenFolders.push(folderName);
 		fs.writeFile(packagePath, packageText, err => {
@@ -127,12 +157,12 @@ function writePackageJsonFiles(callback) {
 	}
 }
 
-function determineWhichPackagesToImportFromGit(callback) {
+function determineWhichPackagesToGitimport(callback) {
 	function getDependencies(pack) {
 		let deps = [];
 		for (let j = 0; j < pack.req.length; ++j) {
 			let depId = pack.req[j];
-			let dep = mapping[depId];
+			let dep = g_packageMapping[depId];
 			if (!dep) {
 				console.log(`missing entry for dependency ${depId}`);
 				continue;
@@ -144,9 +174,9 @@ function determineWhichPackagesToImportFromGit(callback) {
 	/// Returns true if this method needs to be run again, because it added to the list
 	function sortPackagesInsertingDependenciesFirst() {
 		// go through each expected package
-		for (let i = 0; i < requestedPackages.length; ++i) {
-			let packid = requestedPackages[i];
-			let pack = mapping[packid];
+		for (let i = 0; i < g_requestedPackages.length; ++i) {
+			let packid = g_requestedPackages[i];
+			let pack = g_packageMapping[packid];
 			if (!pack) {
 				console.log(_colorRed(`missing package with id ${packid}`));
 				continue;
@@ -157,20 +187,20 @@ function determineWhichPackagesToImportFromGit(callback) {
 			// for each dependency
 			for (let j = 0; j < deps.length; ++j) {
 				// if the dependency is in the list
-				let dependencyIndex = requestedPackages.indexOf(deps[j]);
+				let dependencyIndex = g_requestedPackages.indexOf(deps[j]);
 				if (dependencyIndex >= 0) {
-					// if it's after this expected package in the requestedPackages listing
+					// if it's after this expected package in the g_requestedPackages listing
 					if (dependencyIndex >= i) {
 						// move it to before, and restart this whole process.
-						requestedPackages.splice(dependencyIndex, 1);
-						requestedPackages.splice(i, 0, deps[j]);
+						g_requestedPackages.splice(dependencyIndex, 1);
+						g_requestedPackages.splice(i, 0, deps[j]);
 						return true;
 					}
 				}
 				// if the dependency is not in the list
 				else {
 					// insert it to just before this package entry, and restart this whole process
-					requestedPackages.splice(i, 0, deps[j]);
+					g_requestedPackages.splice(i, 0, deps[j]);
 					return true;
 				}
 			}
@@ -179,41 +209,90 @@ function determineWhichPackagesToImportFromGit(callback) {
 	}
 
 	// filter out duplicates
-	requestedPackages = requestedPackages.filter((x, i) => i === requestedPackages.indexOf(x))
+	g_requestedPackages = g_requestedPackages.filter((x, i) => i === g_requestedPackages.indexOf(x))
 	do{
-		//console.log(_colorCyan(requestedPackages.join(":")));
+		//console.log(_colorCyan(g_requestedPackages.join(":")));
 	} while(sortPackagesInsertingDependenciesFirst());
 
-	for (let i = 0; i < requestedPackages.length; i++) {
-		let value = mapping[requestedPackages[i]];
-		if (value.url) {
-			gitimport += `call git clone ${value.url}\n`;
-		}
-	}
 	callback();
 }
 
 function writeGitimportBatchfile(callback) {
-	fs.writeFile(gitimportFilename, gitimport, err => {
+	let gitImportBatchFile = "cd ..\n";
+	for (let i = 0; i < g_requestedPackages.length; i++) {
+		let value = g_packageMapping[g_requestedPackages[i]];
+		if (value.url) {
+			gitImportBatchFile += `call git clone ${value.url}\n`;
+		}
+	}
+	fs.writeFile(gitImportBatchFilename, gitImportBatchFile, err => {
 		if (err) { console.error(err); reject(err); return; }
-		console.log(`${_colorCyan("wrote")} ${gitimportFilename}`);
+		console.log(`${_colorCyan("wrote")} ${gitImportBatchFilename}`);
 		callback();
 	});
 }
 
+/// adds `g_requestedPackages` to .gitignore
 function writeGitignore(callback) {
-	if (extraLibGitignore.length > 0) {
-		console.log(`adding .gitignore entries:\n${extraLibGitignore}`);
-		let finalAddendum = `# NonStandard library\n${extraLibGitignore}`;
-		fs.appendFile(packageGitignore, finalAddendum, function (err) {
-			if (err) { callback(err); throw err; }
-			console.log(`${_colorCyan("updated")} ${packageGitignore}`);
-			callback();
-		});
-	} else {
-		console.log(_colorGreen(`${packageGitignore} already up to date`));
+	let gitignoreList = null;
+	let foldersToIgnore = [];
+	DoThese([
+		listFoldersToIgnore,
+		loadGitignoreList,
+		writeMissingFoldersToGitignoreFile,
+		c => callback()
+	]);
+	function listFoldersToIgnore(callback) { 
+		for(let i = 0; i < g_requestedPackages.length; ++i) {
+			let packageData = g_packageMapping[g_requestedPackages[i]];
+			if (!packageData) { continue; }
+			let folderName = getFolderName(packageData);
+			if (!folderName) { continue; }
+			foldersToIgnore.push(`${folderName}/`);
+		}
 		callback();
 	}
+	function loadGitignoreList(callback) {
+		fs.readFile(packageGitignore, 'utf8' , (err, data) => {
+			if (err) {
+				console.error(err);
+				callback();
+				return;
+			}
+			gitignoreList = data.split('\n');
+			for(let i = 0; i < gitignoreList.length; i++) {
+				gitignoreList[i] = gitignoreList[i].replace(/\r/g, "");
+			}
+			console.log(`${_colorCyan("found")} ${packageGitignore}`);
+			callback();
+		});
+	}
+	function writeMissingFoldersToGitignoreFile(callback) {
+		let gitignoresToAdd = WhatIsMissing(gitignoreList, foldersToIgnore);
+		if (gitignoresToAdd.length > 0) {
+			console.log(`adding .gitignore entries:\n${gitignoresToAdd}`);
+			let finalAddendum = `# NonStandard library\n${gitignoresToAdd.join('\n')}\n`;
+			fs.appendFile(packageGitignore, finalAddendum, function (err) {
+				if (err) { callback(err); throw err; }
+				console.log(`${_colorCyan("updated")} ${packageGitignore}`);
+				callback();
+			});
+		} else {
+			console.log(_colorGreen(`${packageGitignore} already up to date`));
+			callback();
+		}
+	}
+}
+
+function WhatIsMissing(whatIsAlreadyThere, whatShouldbeAdded) {
+	let whatIsMissing = [];
+	for (let i = 0; i < whatShouldbeAdded.length; ++i) {
+		let entry = whatShouldbeAdded[i];
+		if (whatIsAlreadyThere == null || whatIsAlreadyThere.indexOf(entry) < 0) {
+			whatIsMissing.push(extraIgnoreEntry);
+		}
+	}
+	return whatIsMissing;
 }
 
 // line escape converter, so strings can be passed into other string input
@@ -227,7 +306,7 @@ function getFolderName(packageData) {
 	return fullname.substring(indexSlash+1, indexDot);
 }
 
-function CreatePackageFileData(packageData, mapping) {
+function CreatePackageFileData(packageData) {
 	const infoHeader = "NonStandard library imported using NonStandard package management";
 	let name = packageData.name;
 	let id = packageData.id;
@@ -235,7 +314,7 @@ function CreatePackageFileData(packageData, mapping) {
 	let dependencies = [];
 	if (packageData.req) {
 		for (const [key,value] of Object.entries(packageData.req)) {
-			let dep = mapping[value];
+			let dep = g_packageMapping[value];
 			if (!dep) {
 				console.log(`missing dependency '${value}'`);
 				continue;
